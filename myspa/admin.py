82fbe1bf2dc3fffdc94598bb52c58e47190a80b9
Daniil <daniiltupikin98@gmail.com>
2024-06-04 20:17:32 +0300

    add models SpaCategories, TypeCategories, Composition

--- myspa/admin.py
+++ myspa/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import MassageTherapist, Salon, SpaUser
+from .models import Composition, MassageTherapist, Salon, SpaUser, TypeCategories, SpaСategories
 
 admin.site.register(SpaUser)
 admin.site.register(Salon)
 admin.site.register(MassageTherapist)
+admin.site.register(SpaСategories)
+admin.site.register(TypeCategories)
+admin.site.register(Composition)
--- myspa/models.py
+++ myspa/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
 class SpaUser(AbstractUser):
     phone = models.CharField(max_length=15, blank=True, null=True) 
     profile_image = models.ImageField(upload_to='profile_image/', blank=True, null=True)
@@ -22,4 +22,35 @@ class MassageTherapist(models.Model):
     salon = models.ForeignKey(Salon, on_delete=models.CASCADE, related_name='therapists')
     
     def __str__(self):
-        return f"{self.user.username} {self.salon}" 
+        return f"{self.user.username} {self.salon}"
+    
+class Composition(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return f"{self.name} - {self.description[:40]}"
+
+class TypeCategories(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration = models.DurationField()
+    composition = models.ForeignKey(Composition, on_delete=models.CASCADE, related_name='composition')
+    type_categories_image = models.ImageField(upload_to='type_categories_image/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+
+class SpaСategories(models.Model):
+    name = models.CharField(max_length=100)
+    type_categories = models.ForeignKey(TypeCategories, on_delete=models.CASCADE, related_name='type_categories')
+    categories_image = models.ImageField(upload_to='categories_image/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+    
+
+    
