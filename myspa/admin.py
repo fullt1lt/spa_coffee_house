068314beaf69d09ba03d5c4fa2b3c2ba3d63d937
Daniil <daniiltupikin98@gmail.com>
2025-07-02 20:30:35 +0200

    change settings to deploy 6 static

--- myspa/admin.py
+++ myspa/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 from .models import (BlogAndNews, CafeProduct, Gallery, MassageTherapist, Position, Procedure, Record, Review, Salon, Schedule, SpaUser, TypeBlogAndNews, TypeCafeProduct, TypeCategories, TypeGallery, SpaСategories)
 
 
