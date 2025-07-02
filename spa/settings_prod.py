292ec1405fbc2e5f8e46b769a3977279e13b123d
Daniil <daniiltupikin98@gmail.com>
2025-07-02 19:27:38 +0200

    change settings to deploy 6 static

--- spa/settings_prod.py
+++ spa/settings_prod.py
@@ -18,6 +18,5 @@ DEBUG = False
 ALLOWED_HOSTS = ["13.222.4.243"]
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")  
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_source")] 
+STATIC_ROOT = BASE_DIR / "static"
 MEDIA_ROOT = "media"
