e2c5cf3fb899ba4d9072bf1df1520aa18c0bffec
Daniil <daniiltupikin98@gmail.com>
2025-07-02 20:28:42 +0200

    change settings to deploy 6 static

--- myspa/admin.py
+++ myspa/admin.py
@@ -1,7 +1,52 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import (BlogAndNews, CafeProduct, Gallery, MassageTherapist, Position, Procedure, Record, Review, Salon, Schedule, SpaUser, TypeBlogAndNews, TypeCafeProduct, TypeCategories, TypeGallery, SpaСategories)
 
-admin.site.register(SpaUser)
+
+@admin.register(SpaUser)
+class SpaUserAdmin(UserAdmin):
+    model = SpaUser
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    ordering = ("email",)
+    search_fields = ("email", "first_name", "last_name")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Personal info"),
+            {"fields": ("first_name", "last_name", "phone", "profile_image")},
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "first_name",
+                    "last_name",
+                ),
+            },
+        ),
+    )
+
 admin.site.register(Salon)
 admin.site.register(MassageTherapist)
 admin.site.register(SpaСategories)
